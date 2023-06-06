8ff9772dbac3b075e407f3502087f0439573f04e
mazhelez <mazhelez@microsoft.com>
2023-06-06 23:35:22 +0200

    Fix test jobs generation

--- Actions/CheckForUpdates/CheckForUpdates.ps1
+++ Actions/CheckForUpdates/CheckForUpdates.ps1
@@ -304,31 +304,23 @@ try {
                                 1..$depth | ForEach-Object {
                                     $index = $_-1
 
-                                    # All test job needs to have a dependency on the Initialization job and the previous build jobs
-                                    $needs = @('Initialization', 'Build')
-                                    if ($_ -eq 1) {
-                                        # First test job needs to have a dependency on the Initialization job and the first build job only
-                                        # Example (depth 1):
-                                        #    needs: [ Initialization ]
-                                        #    if: (!failure()) && (!cancelled()) && fromJson(needs.Initialization.outputs.buildOrderJson)[0].projectsCount > 0
-                                        $if = "if: (!failure()) && (!cancelled()) && fromJson(needs.Initialization.outputs.buildOrderJson)[$index].projectsCount > 0"
+                                    # All test job needs to have a dependency on the first build job
+                                    $needs = @('Initialization')
+
+                                    # Subsequent test jobs needs to have a dependency on all previous build jobs
+                                    # Example (depth 2):
+                                    #    needs: [ Initialization, Build1 ]
+                                    #    if: (!failure()) && (!cancelled()) && (needs.Build1.result == 'success' || needs.Build1.result == 'skipped') && fromJson(needs.Initialization.outputs.buildOrderJson)[0].projectsCount > 0
+                                    # Another example (depth 3):
+                                    #    needs: [ Initialization, Build2, Build1 ]
+                                    #    if: (!failure()) && (!cancelled()) && (needs.Build2.result == 'success' || needs.Build2.result == 'skipped') && (needs.Build1.result == 'success' || needs.Build1.result == 'skipped') && fromJson(needs.Initialization.outputs.buildOrderJson)[0].projectsCount > 0
+                                    $newTestJob += @('')
+                                    $ifpart = ""
+                                    ($_)..1 | ForEach-Object {
+                                        $needs += @("Build$_")
+                                        $ifpart += " && (needs.Build$_.result == 'success' || needs.Build$_.result == 'skipped')"
                                     }
-                                    else {
-                                        # Subsequent test jobs needs to have a dependency on all previous build jobs
-                                        # Example (depth 2):
-                                        #    needs: [ Initialization, Build1 ]
-                                        #    if: (!failure()) && (!cancelled()) && (needs.Build1.result == 'success' || needs.Build1.result == 'skipped') && fromJson(needs.Initialization.outputs.buildOrderJson)[0].projectsCount > 0
-                                        # Another example (depth 3):
-                                        #    needs: [ Initialization, Build2, Build1 ]
-                                        #    if: (!failure()) && (!cancelled()) && (needs.Build2.result == 'success' || needs.Build2.result == 'skipped') && (needs.Build1.result == 'success' || needs.Build1.result == 'skipped') && fromJson(needs.Initialization.outputs.buildOrderJson)[0].projectsCount > 0
-                                        $newTestJob += @('')
-                                        $ifpart = ""
-                                        ($_-1)..1 | ForEach-Object {
-                                            $needs += @("Build$_")
-                                            $ifpart += " && (needs.Build$_.result == 'success' || needs.Build$_.result == 'skipped')"
-                                        }
-                                        $if = "if: (!failure()) && (!cancelled())$ifpart && fromJson(needs.Initialization.outputs.buildOrderJson)[$index].projectsCount > 0"
-                                    }
+                                    $if = "if: (!failure()) && (!cancelled())$ifpart && fromJson(needs.Initialization.outputs.buildOrderJson)[$index].projectsCount > 0"
 
                                     # Replace the if:, the needs: and the strategy/matrix/project: in the build job with the correct values
                                     $testJob.Replace('if:', $if)
