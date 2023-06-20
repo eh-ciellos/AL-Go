207a725978004400542baf0719d34c63a18bf0f2
mazhelez <mazhelez@microsoft.com>
2023-06-20 14:46:55 +0200

    Calculate artifact URL

--- Actions/GetBCEnvironment/GetBCEnvironment.ps1
+++ Actions/GetBCEnvironment/GetBCEnvironment.ps1
@@ -14,6 +14,32 @@ $bcContainerHelperPath = $null
 
 # IMPORTANT: No code that can fail should be outside the try/catch
 
+function Get-BCArtifactUrlBasedOnSettings($artifact) {
+    # Taken from BC Container Helper
+
+    $segments = "$artifact/////".Split('/')
+
+    $storageAccount = $segments[0];
+    $type = $segments[1]; if ($type -eq "") { $type = 'Sandbox' }
+    $version = $segments[2]
+    $country = $segments[3]; if ($country -eq "") { $country = "us" }
+    $select = $segments[4]; if ($select -eq "") { $select = "latest" }
+    $sasToken = $segments[5]
+
+    Write-Host "Determining artifacts to use"
+    $minsto = $storageAccount
+    $minsel = $select
+    $mintok = $sasToken
+
+    $artifactUrl = Get-BCArtifactUrl -storageAccount $minsto -type $type -version $version -country $country -select $minsel -sasToken $mintok | Select-Object -First 1
+    
+    if (!($artifactUrl)) {
+        throw "Unable to locate artifacts"
+    }
+
+    return $artifactUrl
+}
+
 try {
     . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
     function Get-BCDockerCredentials {
@@ -36,12 +62,13 @@ try {
         
         Write-Host "Creaing docker container"
 
-        $repoSettings = ReadSettings -baseFolder $BaseFolder -project $Project
+        $settings = ReadSettings -baseFolder $BaseFolder -project $Project
+        $artifactUrl = Get-BCArtifactUrlBasedOnSettings $settings.artifact $settings.additionalCountries
 
         $Parameters = @{
             "accept_eula" = $true
             "containerName" = $containerName
-            "artifactUrl" = $($repoSettings.artifactUrl)
+            "artifact" = $artifactUrl
             "useGenericImage" = $useGenericImage
             "Credential" = $credential
             "auth" = 'UserPassword'
