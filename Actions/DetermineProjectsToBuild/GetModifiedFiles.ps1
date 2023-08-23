ceec548289171840f07edbbaf2086c47eb3c1c47
eh-ciellos <124346996+eh-ciellos@users.noreply.github.com>
2023-08-23 10:40:47 +0200

    Update GetModifiedFiles.ps1 add on push and merged

--- Actions/DetermineProjectsToBuild/GetModifiedFiles.ps1
+++ Actions/DetermineProjectsToBuild/GetModifiedFiles.ps1
@@ -10,8 +10,8 @@ if(!$env:GITHUB_EVENT_PATH) {
 
 $ghEvent = Get-Content $env:GITHUB_EVENT_PATH -Encoding UTF8 | ConvertFrom-Json
 
-if(-not ($ghEvent.PSObject.Properties.name -eq 'pull_request') -or ($ghEvent.PSObject.Properties.name -eq 'push')) {
-    Write-Host "Not a pull request, returning empty list of changed files"
+if(-not ($ghEvent.PSObject.Properties.name -eq 'pull_request') -or ($ghEvent.PSObject.Properties.name -eq 'pull_request.merged') -or ($ghEvent.PSObject.Properties.name -eq 'push')) {
+    Write-Host "Not a pull request, merge nor push returning empty list of changed files"
     return @()
 }
 
